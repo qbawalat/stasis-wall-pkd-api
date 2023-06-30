443ac6833a6d63f2a1c43976222979effdfcc8cc
Qba Walat <kubawalat@gmail.com>
2023-06-30 19:29:02 +0200

    test: :white_check_mark: add controller tests

--- src/pkd-query/pkd-query.controller.spec.ts
+++ src/pkd-query/pkd-query.controller.spec.ts
@@ -1,18 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PkdQueryController } from './pkd-query.controller';
+import { PkdQueryService } from './pkd-query.service';
 
 describe('PkdQueryController', () => {
   let controller: PkdQueryController;
+  let service: PkdQueryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PkdQueryController],
+      providers: [PkdQueryService],
     }).compile();
 
     controller = module.get<PkdQueryController>(PkdQueryController);
+    service = module.get(PkdQueryService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should inject pkd-query service', () => {
+    expect(service).toBeDefined();
   });
 });
--- src/pkd-query/pkd-query.controller.ts
+++ src/pkd-query/pkd-query.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { PkdQueryService } from './pkd-query.service';
 
 @Controller('pkd-query')
-export class PkdQueryController {}
+export class PkdQueryController {
+  constructor(private readonly pkdQueryService: PkdQueryService) {}
+}
