f19c7d242bc1cd4f095c5eac9a198c644b0b881c
Kuba Walat <35037208+qbawalat@users.noreply.github.com>
2023-07-03 18:23:42 +0200

    feat: :card_file_box: add mongoose db (#6)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PkdQueryModule } from './pkd-query/pkd-query.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PkdQueryModule],
+  imports: [PkdQueryModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
